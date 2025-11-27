e0cd22cc24201e17e6d2699ff18a3fb45d60a562
MintyU <mintyu.dev@gmail.com>
2025-11-27 17:22:35 +0900

    Lecture 5

--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -1,10 +1,17 @@
 package com.back;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
+    private final PersonRepository personRepository;
+
+    public PersonService(@Qualifier("personRepository2") PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
     public long count() {
-        return 3;
+        return personRepository.count();
     }
 }
